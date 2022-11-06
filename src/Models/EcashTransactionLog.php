20bf1208e475686137b7d1f60cda8f6deb64112b
Eyad Bereh <eyadbere@gmail.com>
2022-11-06 03:06:26 +0200

    Removed HasFactory trait

--- src/Models/EcashTransactionLog.php
+++ src/Models/EcashTransactionLog.php
@@ -2,13 +2,10 @@
 
 namespace IXCoders\LaravelEcash\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class EcashTransactionLog extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'checkout_type',
         'amount',
