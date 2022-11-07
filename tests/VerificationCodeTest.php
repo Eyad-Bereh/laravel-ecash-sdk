95dbc5908a32d1351447955205bffd0caeab057a
Eyad Bereh <eyad@ixcoders.com>
2022-11-07 18:18:11 +0300

    Initial commit

---
+++ tests/VerificationCodeTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace IXCoders\LaravelEcash\Tests;
+
+class VerificationCodeTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
