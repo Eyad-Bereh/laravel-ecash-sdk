9a1e344dc7deb4e84e34e1bdd04c0af500263716
Eyad Bereh <eyadbere@gmail.com>
2022-11-10 02:52:44 +0300

    Changed parameter name from (alternative) to (alias)

--- src/Exceptions/MissingMiddlewareException.php
+++ src/Exceptions/MissingMiddlewareException.php
@@ -6,11 +6,11 @@ use Exception;
 
 class MissingMiddlewareException extends Exception
 {
-    public function __construct(string $route, string $middleware, ?string $alternative = NULL)
+    public function __construct(string $route, string $middleware, ?string $alias = NULL)
     {
         $message = "The route ($route) should use the middleware ($middleware).";
-        if (!is_null($alternative)) {
-            $message .= "This middleware is named as ($alternative).";
+        if (!is_null($alias)) {
+            $message .= "This middleware is named as ($alias).";
         }
         parent::__construct($message);
     }
