fa5b484c9b59368bb98620ec762349b2c6e6b66b
Eyad Bereh <eyadbere@gmail.com>
2022-11-09 04:29:27 +0300

    Added constraints on routes methods

---
+++ src/Exceptions/InvalidRouteMethodException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace IXCoders\LaravelEcash\Exceptions;
+
+use Exception;
+
+class InvalidRouteMethodException extends Exception
+{
+    public function __construct(string $route_name, string $method, array $current_methods)
+    {
+        $methods = implode(", ", $current_methods);
+        $message = "The route defined with the name ($route_name) should support the method ($method), but it currently supports the method(s) ($methods).";
+        parent::__construct($message);
+    }
+}
